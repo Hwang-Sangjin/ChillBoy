fb622342868708b9e90cfebaf9fa8f9192fa38cb
Sangjin Hwang <aosdb5640@naver.com>
2025-02-05 16:00:53 +0900

    add: footer, audio controller

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -2,16 +2,28 @@ import { useRecoilState } from "recoil";
 import startState from "../recoil/startState";
 import { useEffect, useRef } from "react";
 import ChillMusic from "/ChillMusic.mp3";
+import musicState from "../recoil/musicState";
 
 const Audio = () => {
   const [start, setStart] = useRecoilState(startState);
+  const [music, setMusic] = useRecoilState(musicState);
   const audioRef = useRef();
 
   useEffect(() => {
-    if (start === false) {
+    if (start === false && audioRef.current) {
       audioRef.current.play();
     }
   }, [start]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      if (music) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [music]);
 
   return <audio ref={audioRef} src={ChillMusic} loop />;
 };
--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,9 +1,26 @@
 import Footer from "./Footer";
-
+import { HiOutlineVolumeUp } from "react-icons/hi";
+import { HiOutlineVolumeOff } from "react-icons/hi";
+import { useRecoilState } from "recoil";
+import musicState from "../recoil/musicState";
+import { useEffect } from "react";
 const UI = () => {
+  const [music, setMusic] = useRecoilState(musicState);
+
+  const toggleMusic = () => {
+    setMusic((prev) => !prev);
+  };
+
   return (
     <>
       <Footer />
+      <div className="absolute  bottom-40 right-5 px-5 py-2">
+        {music ? (
+          <HiOutlineVolumeUp onClick={() => toggleMusic()} size={40} />
+        ) : (
+          <HiOutlineVolumeOff onClick={() => toggleMusic()} size={40} />
+        )}
+      </div>
     </>
   );
 };
